b216e4ec4a3ca91bcf703fba582ef1d1038d9191
zhangsan <18689496617@163.com>
2021-12-31 15:56:18 +0800

    redis配置

--- alibaba-cloud-study/cloud-order-service/src/main/java/com/limai/order/controller/TestController.java
+++ alibaba-cloud-study/cloud-order-service/src/main/java/com/limai/order/controller/TestController.java
@@ -4,8 +4,33 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/nacostest")
 @RefreshScope
 public class TestController {
+    public static void main(String[] args) {
+        String ss ="aabcddee";
+        String str="";
+        List<String> list = new ArrayList<>();
+        for (int j = 0; j < ss.length();j++) {
+            if(str.contains(ss.charAt(j)+"")){
+                list.add(str);
+                if(j==ss.length()-1){
+                    list.add(ss.charAt(j)+"");
+                }else{
+                    str = ss.charAt(j)+"";
+                }
+
+            }else{
+                str=str+ss.charAt(j);
+            }
+        }
+        for(String sstr:list){
+            System.out.println(sstr);
+        }
+    }
 }
+
